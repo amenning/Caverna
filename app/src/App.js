55b8de7be45508dc7248c5305d969ae5268243cf
Andrew Menning <menning23@gmail.com>
2019-09-15 18:07:58 -0400

    Add background image and update favicon.ico

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+import classes from './App.css';
+
 import Layout from './hoc/Layout/Layout';
 import GameSetup from './containers/GameSetup/GameSetup';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className={classes.App}>
         <Layout>
           <Switch>
             {/* <Route path="/game" component={Game} */}
