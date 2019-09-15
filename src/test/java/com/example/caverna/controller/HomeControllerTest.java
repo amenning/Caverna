33b5206022a7a01f26a85eea656a5d397209be66
Andrew Menning <menning23@gmail.com>
2019-09-15 15:59:14 -0400

    Clean up imports

--- src/test/java/com/example/caverna/controller/HomeControllerTest.java
+++ src/test/java/com/example/caverna/controller/HomeControllerTest.java
@@ -1,11 +1,5 @@
 package com.example.caverna.controller;
 
-import static org.hamcrest.core.StringContains.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
-import javax.xml.xpath.XPath;
-
 import com.example.caverna.CavernaApplication;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,10 +9,12 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
