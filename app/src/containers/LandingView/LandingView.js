e146871ecd901ec6d85b6fc8e523fa8a85446fd4
Andrew Menning <menning23@gmail.com>
2019-09-16 22:41:29 -0400

    Add play button to landing page to take user to setup view

--- app/src/containers/LandingView/LandingView.js
+++ app/src/containers/LandingView/LandingView.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 
+import classes from './LandingView.css';
 import Aux from '../../hoc/Aux/Aux';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import CoverBox from '../../components/CoverBox/CoverBox';
+import Button from '../../components/UI/Button/Button';
 
 class LandingView extends Component {
   state = {}
 
-  componentDidMount () {
+  nextPath(path) {
+    this.props.history.push(path);
   }
 
   render () {
     return (
       <Aux>
-        <CoverBox />
+        <div className={classes.LandingView}>
+            <CoverBox />
+            <Button btnType="Success" clicked={() => this.nextPath('/GameSetup')}>Play</Button>
+        </div>
       </Aux>
     );
   };
@@ -30,4 +37,4 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(LandingView));
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(withErrorHandler(LandingView)));
