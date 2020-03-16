5c286deb714317b9171eba191837a36f8838db2d
Andrew Menning <menning23@gmail.com>
2020-03-16 01:20:10 -0400

    Allow for dynamic loading of hashed css file

--- src/main/java/com/example/caverna/controller/HomeController.java
+++ src/main/java/com/example/caverna/controller/HomeController.java
@@ -1,8 +1,12 @@
 package com.example.caverna.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.io.File;
+import java.io.FilenameFilter;
 
 @Controller
 public class HomeController {
@@ -10,7 +14,17 @@ public class HomeController {
         value = "/",
         method = RequestMethod.GET
     )
-    public String index() {
+    public String index(Model model) {
+        // Grab main css file with hashed name
+        File f = new File("src/main/resources/static/css");
+        File[] matchingFiles = f.listFiles(new FilenameFilter() {
+            public boolean accept(File dir, String name) {
+                return name.startsWith("main") && name.endsWith("css");
+            }
+        });
+        String mainCssFilename = "/css/" + matchingFiles[0].getName();
+        model.addAttribute("mainCssFilename", mainCssFilename);
+
         return "index";
     }
 }
