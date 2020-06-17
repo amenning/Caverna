38f6e2366090ce26a7209666c9022ea5b1cd8a24
Andrew Menning <menning23@gmail.com>
2020-06-17 19:05:40 -0400

    Add resource track component and update css for resource markers

--- app/src/containers/CaveBoard/CaveBoard.js
+++ app/src/containers/CaveBoard/CaveBoard.js
@@ -5,13 +5,8 @@ import Aux from '../../hoc/Aux/Aux';
 import caveBoard from '../../assets/images/CaveBoard/CaveBoard.jpg';
 import RoomTile from '../../components/RoomTile/RoomTile';
 import grayRoomTileBack from '../../assets/images/RoomTiles/DarkGrayRoomTileBacks.jpg';
-import ResourceMarker from '../../components/ResourceMarker/ResourceMarker';
-import flaxImage from '../../assets/images/ResourceMarker/Flax.png';
-import emmerImage from '../../assets/images/ResourceMarker/Emmer.png';
-import stoneImage from '../../assets/images/ResourceMarker/Stone.png';
-import woodImage from '../../assets/images/ResourceMarker/Wood.png';
-import foodImage from '../../assets/images/ResourceMarker/Food.png';
-import goldImage from '../../assets/images/ResourceMarker/Gold.png';
+
+import ResourceTrack from './ResourceTrack';
 
 /**
  * The cave board room tile placement numbers are as follows:
@@ -59,24 +54,7 @@ class CaveBoard extends Component {
             <div className={classes.RoomTileTen}>
                 <RoomTile roomTileImage={grayRoomTileBack} roomTileImageAlt='test' />
             </div>
-            <div className={classes.FlaxMarker}>
-                <ResourceMarker resourceMarkerImage={flaxImage} resourceMarkerImageAlt='flax' />
-            </div>
-            <div className={classes.EmmerMarker}>
-                <ResourceMarker resourceMarkerImage={emmerImage} resourceMarkerImageAlt='emmer' />
-            </div>
-            <div className={classes.StoneMarker}>
-                <ResourceMarker resourceMarkerImage={stoneImage} resourceMarkerImageAlt='stone' />
-            </div>
-            <div className={classes.WoodMarker}>
-                <ResourceMarker resourceMarkerImage={woodImage} resourceMarkerImageAlt='wood' />
-            </div>
-            <div className={classes.FoodMarker}>
-                <ResourceMarker resourceMarkerImage={foodImage} resourceMarkerImageAlt='food' />
-            </div>
-            <div className={classes.GoldMarker}>
-                <ResourceMarker resourceMarkerImage={goldImage} resourceMarkerImageAlt='gold' />
-            </div>
+            <ResourceTrack />
         </div>
       </Aux>
     );
