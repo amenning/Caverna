b33e0b3e492358eab4afaf19c9307b3e161b6469
Andrew Menning <menning23@gmail.com>
2020-06-17 18:04:00 -0400

    Create resource state store

--- app/src/store/reducer.js
+++ app/src/store/reducer.js
@@ -1,6 +1,14 @@
 import * as actionTypes from './actions';
 
 const initialState = {
+    resources: {
+        emmer: 1,
+        flax: 1,
+        food: 1,
+        gold: 1,
+        stone: 1,
+        wood: 1
+    }
 };
 
 const reducer = (state = initialState, action) => {
