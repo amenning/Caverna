dcdac81e2ae4b92766562e70e8b8c922fdf0dcda
Andrew Menning <menning23@gmail.com>
2020-07-01 23:43:17 -0400

    Add new tables

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,42 @@
+INSERT INTO player (first_name)
+SELECT *
+FROM (
+    SELECT 'Andrew' AS first_name
+) x
+WHERE NOT EXISTS (
+    SELECT *
+    FROM player
+);
+COMMIT;
+
+INSERT INTO game (player_id)
+SELECT *
+FROM (
+    SELECT player_id
+    FROM player
+    WHERE first_name = 'Andrew'
+) x
+WHERE NOT EXISTS (
+    SELECT *
+    FROM game
+);
+COMMIT;
+
+INSERT INTO cave (game_id, player_id)
+SELECT *
+FROM (
+    SELECT g.game_id
+        ,p.player_id
+    FROM game g
+        LEFT JOIN player p ON g.player_id = p.player_id
+    WHERE p.first_name = 'Andrew'
+) x
+WHERE NOT EXISTS (
+    SELECT *
+    FROM cave
+);
+COMMIT;
+
 INSERT INTO room_tile (room_tile_name)
 SELECT *
 FROM (
