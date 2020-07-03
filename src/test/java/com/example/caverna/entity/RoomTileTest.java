665a4c15623c10f16ad4f3fb58b1a6b55cc26ed8
Andrew Menning <menning23@gmail.com>
2020-07-03 10:47:20 -0400

    Add travis config

---
+++ src/test/java/com/example/caverna/entity/RoomTileTest.java
@@ -0,0 +1,31 @@
+package com.example.caverna.entity;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoomTileTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getRoomTileId() {
+        assertTrue(true);
+    }
+
+    @Test
+    void getRoomTileName() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
